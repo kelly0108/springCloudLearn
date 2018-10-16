f65ccd15dc681c3bce8150d15118a057de713bdd
guohong <guohong@huatu.com>
2018-10-16 17:11:10 +0800

    client

---
+++ service-ribbon/src/main/java/com/forezp/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,30 @@
+package com.forezp.serviceribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//基于spring-cloud-netflix依赖，只能为eureka作用
+@EnableEurekaClient
+//基于spring-cloud-commons依赖，并且在classpath中实现
+@EnableDiscoveryClient
+//如果选用的注册中心是eureka，那么就推荐@EnableEurekaClient，如果是其他的注册中心，那么推荐使用@EnableDiscoveryClient。
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
